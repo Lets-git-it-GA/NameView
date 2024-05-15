a89714b938cfc97b0e96edcc7f4abfe2a2d4460d
Junsung Kim <assd1108@naver.com>
2024-05-16 03:07:22 +0900

    Initial Commit

---
+++ NameView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NameView
+//
+//  Created by 김준성 on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NameView/NameViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameViewApp.swift
+//  NameView
+//
+//  Created by 김준성 on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NameViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
