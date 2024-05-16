99b3478fcb034abd357fcc07b24510d061e65cda
Ho Mun <mhhh2082@gmail.com>
2024-05-16 16:39:04 +0900

    [Feat] `ShiroView` 구현 (#5)

---
+++ NameView/ShiroView.swift
@@ -0,0 +1,22 @@
+//
+//  ShiroView.swift
+//  NameView
+//
+//  Created by 문호 on 5/16/24.
+//
+
+import SwiftUI
+
+struct ShiroView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "sun.min")
+            Text("문시로")
+        }
+        
+    }
+}
+
+#Preview {
+    ShiroView()
+}
